42ebe710e4149631b918f357539b39c096042896
ltl3A87 <t29li@uwaterloo.ca>
2024-11-23 08:19:10 -0500

    updated

--- llm/api/gemini_models.py
+++ llm/api/gemini_models.py
@@ -7,18 +7,12 @@ from llm.format import *
 from utils.global_functions import *
 
 import google.generativeai as genai
-from vertexai.generative_models import GenerativeModel, Part, GenerationConfig
-import vertexai
 GOOGLE_API_KEY = os.getenv(f"GOOGLE_API_KEY")
 genai.configure(api_key=GOOGLE_API_KEY)
-PROJECT_ID = "mllm-gemini"  # @param {type:"string"}
-LOCATION = "us-central1"
 
 class GeminiChat(LLMChat):
     def __init__(self, model_name: str):
         self.model_name = model_name
-        vertexai.init(project=PROJECT_ID, location="us-central1")
-        self.model = GenerativeModel(self.model_name)
         
     def get_msg(self, messages: List[Message]) -> List[dict]:
         return [msg.to_gemini_format() for msg in messages]
@@ -36,22 +30,17 @@ class GeminiChat(LLMChat):
         max_tokens = max_tokens if max_tokens else self.DEFAULT_MAX_TOKENS
         num_comps = num_comps if num_comps else self.DEFAULT_NUM_COMPLETIONS
         inputs = self.get_msg(messages)
-        inputs = messages[1].content
-
-        # vertexai.init(project=PROJECT_ID, location=LOCATION)
-        response = self.model.generate_content(
+        
+        model = genai.GenerativeModel(self.model_name)
+        response = model.generate_content(
             inputs,
-            generation_config=GenerationConfig(
+            generation_config=genai.types.GenerationConfig(
                 candidate_count=num_comps,
                 max_output_tokens=max_tokens,
-                temperature=temperature),
-        stream = False
-        )
-
-        # for response in responses:
+                temperature=temperature)
+        )   
+        
+        time.sleep(self.DEFAULT_DELAY)
+        
         return response.text
-        
-        # time.sleep(self.DEFAULT_DELAY)
-        
-        # return response.text
     
